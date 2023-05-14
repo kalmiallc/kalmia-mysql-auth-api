6e7a50a5de6f1affd1e8b5d8ffb0e5f10f4962e2
bterpinc <borut.terpinc@kalmia.si>
2023-05-14 22:12:53 +0200

    Version  1.0.11

--- dist/config/env.js
+++ dist/config/env.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.env = void 0;
 /* eslint-disable radix */
@@ -12,5 +13,5 @@ exports.env = Object.assign(Object.assign({}, kalmia_sql_lib_1.env), {
     /*
      * App secret for JWT.
      */
-    APP_SECRET: process.env['APP_SECRET'] || 'notasecret', RSA_JWT_PK: process.env['RSA_JWT_PK'].replace(/#/g, '\n') || undefined });
+    APP_SECRET: process.env['APP_SECRET'] || 'notasecret', RSA_JWT_PK: ((_a = process.env['RSA_JWT_PK']) === null || _a === void 0 ? void 0 : _a.replace(/#/g, '\n')) || undefined });
 //# sourceMappingURL=env.js.map
